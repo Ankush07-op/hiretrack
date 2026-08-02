e20912123aa3b0811da51275a22fc9d4940cc1b7
Ankush Pandey <135643189+Ankush07-op@users.noreply.github.com>
2026-08-03 02:09:26 +0530

    Added Resume view API

--- src/app/api/resumes/route.ts
+++ src/app/api/resumes/route.ts
@@ -104,3 +104,76 @@ export async function POST(request: Request) {
     );
   }
 }
+
+// Returns all resumes of the logged-in candidate.
+// This endpoint allows a candidate to view all of their resumes.
+export async function GET(request: Request) {
+  try{
+
+    // Authentication
+    const session = await auth();
+    
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "Login required.",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    // Authorization
+    if (session.user.role !== Role.CANDIDATE) {
+      return NextResponse.json(
+        {
+          message: "Only candidates are allowed to view their created resumes.",
+        },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const resumes = await prisma.resume.findMany({
+      where: {
+        candidateId: session.user.id,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      select: {
+        id: true,
+        title: true,
+        summary: true,
+        githubUrl: true,
+        linkedinUrl: true,
+        resumeUrl: true,
+        createdAt: true,
+        updatedAt: true,
+      }
+    });
+
+    return NextResponse.json(
+      {
+        resumes,
+      },
+      {
+        status: 200,
+      }
+    );
+
+  } catch(error){
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Something went wrong.",
+      },
+    {
+      status: 500,
+    }
+    );
+  }
+}
